2a828ff29164a7db44056de04c0c21f5b3a66782
Sameh Mohamed <samehmimo987@gmail.com>
2026-07-26 03:28:15 +0300

    Frist upload

---
+++ js/app.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    renderHome();
+});
+
+window.addEventListener("scroll", toggleScrollBtn);
+
+window.addEventListener("resize", () => {
+    // إجراءات إضافية عند تغيير حجم النافذة إن وجدت
+});
