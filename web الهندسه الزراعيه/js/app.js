5ae4ce68f751ade0d5b0d71a8e989ee8211bb146
Sameh Mohamed <samehmimo987@gmail.com>
2026-07-26 03:01:56 +0300

    First upload

--- web الهندسه الزراعيه/js/app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    renderHome();
-});
-
-window.addEventListener("scroll", toggleScrollBtn);
-
-window.addEventListener("resize", () => {
-    // إجراءات إضافية عند تغيير حجم النافذة إن وجدت
-});
